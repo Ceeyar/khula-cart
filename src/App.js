abfa050d0fa839880c988fd443c4d3e9f77d9759
siya_ndovela <siyabonga@sov.tech>
2019-10-20 16:42:23 +0200

    Added sass

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import Header from './components/header-nav/Header';
+import AppNavigation from './navigation/AppNavigation';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header/>
+        <AppNavigation/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
